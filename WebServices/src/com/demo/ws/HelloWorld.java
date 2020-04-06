54856833e2cd714710c55cacc45a2430b8fcd027
Swathi Doma <swathidoma@Swathis-MacBook-Pro.local>
2020-04-06 18:51:35 -0500

    Soap webservice

---
+++ WebServices/src/com/demo/ws/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.demo.ws;
+ 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
+ 
+//Service Endpoint Interface
+@WebService
+@SOAPBinding(style = Style.DOCUMENT, use=Use.LITERAL)
+public interface HelloWorld{
+ 
+	@WebMethod String getHelloWorldAsString(String name);
+	
+
+} 
