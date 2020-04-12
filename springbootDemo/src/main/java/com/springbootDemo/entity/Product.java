f9da98502cc449cf6f0a2ba1564e06f19d98f024
Swathi Doma <swathidoma@Swathis-MacBook-Pro.local>
2020-04-11 19:10:37 -0500

    spring boot samples

---
+++ springbootDemo/src/main/java/com/springbootDemo/entity/Product.java
@@ -0,0 +1,77 @@
+package com.springbootDemo.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //@NonNull for the RequiredArgConstructor
+    @NonNull private String name;
+    @NonNull private String description;
+    @NonNull private double price;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @UpdateTimestamp
+    private Date updatedAt;
+
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}    
+
+}
