b6610f38c0c2ab6ded97dc98a818ba88ab2ae62e
Swathi Doma <swathidoma@Swathis-MacBook-Pro.local>
2020-04-13 19:10:35 -0500

    springsecurity demo

---
+++ polling-app-server/src/main/java/com/example/polls/model/RoleName.java
@@ -0,0 +1,9 @@
+package com.example.polls.model;
+
+/**
+ * Created by Brahmaiah Doma on 04/12/20.
+ */
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ polling-app-server/src/main/java/com/example/polls/payload/JwtAuthenticationResponse.java
@@ -0,0 +1,29 @@
+package com.example.polls.payload;
+
+/**
+ * Created by Brahmaiah Doma on 04/12/20.
+ */
+public class JwtAuthenticationResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public JwtAuthenticationResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+}
